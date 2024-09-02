77ea28bb1734cdbf01990d8c92e398de03fa5088
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-09-03 00:16:04 +0300

    сцепление строк

--- Глава 2/Воображаемые благодарности (fancy_credits).py
+++ Глава 2/Воображаемые благодарности (fancy_credits).py
@@ -1,17 +1,17 @@
 # Fancy Credits
-# Demonstrates escape sequences
+# Образец применения Escape-последовательностей
 
 print("\t\t\tFancy Credits")
 
-print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\") # три табуляционных отступа
 print("\t\t\t\tby")
 print("\t\t\tMichael Dawson")
-print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\") # два обратных слеша "\\" питон видит как один.
 
 print("\nSpecial thanks goes out to:")
-print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
-
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"") # вывод буквального апострофа выглядит так: \'
+# а кавычка в общем виде выглядит так: \"
 # sound the system bell
-print("\a")
+print("\a") # заставляет звучать системный динамик
 
 input("\n\nPress the enter key to exit.")
